06172ca5400add1e0e7041acb36e7669b98fb78b
Dapilah Sydney <51008616+SydDaps@users.noreply.github.com>
2024-01-21 23:34:33 +0000

    cli updates

--- lib/bianchi/generator/cli.rb
+++ lib/bianchi/generator/cli.rb
@@ -3,9 +3,11 @@ require 'thor'
 module Bianchi
   module Generator
     class Cli < Thor
-      desc "say_hello Name", "an example task"
-      def say_hello(name)
-        puts "hello #{name}"
+      desc "-v", "Show bianchi version number"
+      map %w[-v --version] => :version
+      # USAGE: bianchi -v
+      def version
+        say "Bianchi #{Bianchi::VERSION}"
       end
 
       def self.exit_on_failure?
