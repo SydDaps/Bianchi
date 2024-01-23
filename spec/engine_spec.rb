2ad878ce1da9077f640f8dc84807bf9b7941b28c
Dapilah Sydney <51008616+SydDaps@users.noreply.github.com>
2024-01-23 03:24:37 +0000

    engine render tests

--- spec/engine_spec.rb
+++ spec/engine_spec.rb
@@ -1,6 +1,26 @@
 # frozen_string_literal: true
 
 RSpec.describe Bianchi::USSD::Engine do
+  let(:correct_params) do
+      {
+        mobile_number: "+233557711911",
+        activity_state: "initial",
+        session_id: "345344322123",
+        input_body: ""
+      }
+  end
+
+
+  let(:menu) { Bianchi::USSD::Menu.new "main" }
+  let(:session) do
+    session = Bianchi::USSD::Session.new correct_params
+    session.menu = menu
+
+    session
+  end
+
+  let(:page) { Bianchi::USSD::Page.new session }
+
   context "validation" do
     it "raises argument error when invalid parameters are passed" do
       params = {}
@@ -12,32 +32,29 @@ RSpec.describe Bianchi::USSD::Engine do
     end
 
     it "raises page load error when initial menu isn't defined" do
-      params = {
-        mobile_number: "+233557711911",
-        activity_state: "initial",
-        session_id: "345344322123",
-        input_body: ""
-      }
-
-      expect{ Bianchi::USSD::Engine.start(params) {}}.to raise_error do |error|
+      expect{ Bianchi::USSD::Engine.start(correct_params) {}}.to raise_error do |error|
         expect(error).to be_a(Bianchi::USSD::PageLoadError)
         expect(error.message).to eq "an initial menu is required to proceed"
       end
     end
 
     it "raises page load error when initial menu is created but page files cant be found" do
-      params = {
-        mobile_number: "+233557711911",
-        activity_state: "initial",
-        session_id: "345344322123",
-        input_body: ""
-      }
-
-      expect{ Bianchi::USSD::Engine.start(params) { menu :main, initial: true} }.to raise_error do |error|
+      expect{ Bianchi::USSD::Engine.start(correct_params) { menu :main, initial: true} }.to raise_error do |error|
         expect(error).to be_a(Bianchi::USSD::PageLoadError)
         expect(error.message).to eq "USSD::MainMenu::Page1 is supposed to be defined to process main menu Page1"
       end
     end
   end
 
+  context "Operation" do
+    it "renders am initial menu pages request" do
+      stub_const 'USSD::MainMenu::Page1', instance_double('USSD::MainMenu::Page1', new: page)
+      allow(page).to receive(:response).and_return( page.render_and_end "test")
+      allow(page).to receive(:request).and_return( page.render_and_await "test")
+
+
+      engine_object = Bianchi::USSD::Engine.start(correct_params) { menu :main, initial: true}
+      expect(engine_object.prompt_data).to eq({"activity_state"=>:await, "body"=>"test", "mobile_number"=>"+233557711911", "session_id"=>"345344322123"})
+    end
+  end
 end
